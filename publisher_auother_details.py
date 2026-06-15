f79a2f1312bd2034ced5a44d0a5737bdc67237d4
meghana s megha <meghanas303@gmail.com>
2026-06-15 22:28:41 +0530

    adding python program of publisher_auother_details.py

---
+++ publisher_auother_details.py
@@ -0,0 +1,74 @@
+publisher_name="priya"
+age=86
+auother_name="meghana"
+auother_age=67
+books_published=109
+published_name=input("enter your name : ")
+enter your name : >? priya
+print(published_name)
+priya
+publisher_age=86
+publisher_age=input("enter your age : ")
+enter your age : >? 86
+print(publisher_age)
+86
+print("enter auother name : ")
+enter auother name : 
+meghana
+print("enter auother name : ")
+enter auother name : 
+print(auother_name)
+meghana
+print(auother_age)
+67
+publisher_city="kolkata"
+print("i am from" , publisher_city)
+i am from kolkata
+auother_city="bengalore"
+print("i am from" , auother_city)
+i am from bengalore
+book_name="mystery of moon"
+book_name=input("enter your book_name")
+enter your book_name>? print(book_name)
+print(" my name is " +  publisher_name  + " i am from " + publisher_city  + " i publised a book called " +  book_name )
+ my name is priya i am from kolkata i publised a book called print(book_name)
+book_name="the hidden key"
+print(book_name)
+the hidden key
+print(" my name is " +  publisher_name  + " i am from " + publisher_city  + " i publised a book called " +  book_name )
+ my name is priya i am from kolkata i publised a book called the hidden key
+year=2025
+print("year is : " , year)
+print(" in the year  " + str(year))
+ in the year  2025
+print(" my name is " + auother_name + " i am from " + auother_city + " i wrote a book named is " + book_name + " year of "  , str(year))
+my name is meghana i am from bengalore i wrote a book named is the hidden key in the year of  2025 
+ 
+publisher_name="priya"
+publisher_name=(name.upper())
+publisher_name="priya"
+print(publisher_name)
+priya
+publisher_name=(publisher_name.upper())
+print(publisher_name)
+PRIYA
+publisher_name=input(publisher_name.upper()*5)
+PRIYAPRIYAPRIYAPRIYAPRIYA
+auother_name="meghana"
+print(auother_name)
+meghana
+print(auother_name.capitalize())
+Meghana
+book_name="the hidden key"
+print(book_name)
+the hidden key
+print(book_name.replace("the hidden key" , "mystray of moon"))
+mystray of moon
+print(len(auother_name))
+7
+print('''my name is  meghana 
+      i am from kolkata 
+      i wrote a book called the hidden key ''')
+my name is  meghana 
+      i am from kolkata 
+      i wrote a book called the hidden key 
